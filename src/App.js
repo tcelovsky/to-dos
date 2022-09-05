6bfc3f1f02a3a24bb8c87c15c46543e1437d4b34
tscotce <tatyana.celovsky@gmail.com>
2022-09-05 15:41:16 -0400

    Create basic input field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React, { useState } from "react";
+import { TextField, Button } from "@mui/material";
 
 function App() {
   const [todos, setTodos] = useState(["Build a React App", "Write a blogpost"]);
@@ -7,7 +8,18 @@ function App() {
 
   return (
     <div className="App">
-      <h2>To Dos</h2>
+      <h2>To Do List</h2>
+      <form>
+        <TextField
+          id="outlined-basic"
+          label="Create Item"
+          variant="outlined"
+          style={{ margin: "0px 5px" }}
+          size="small"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></TextField>
+      </form>
     </div>
   );
 }
