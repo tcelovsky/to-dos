3025ef27f339ee545a4a615c702460cfd4c1666d
tscotce <tatyana.celovsky@gmail.com>
2022-09-05 15:37:39 -0400

    Set up initial state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import React, { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState(["Build a React App", "Write a blogpost"]);
+  const [input, setInput] = useState("");
+
   return (
     <div className="App">
       <h2>To Dos</h2>
