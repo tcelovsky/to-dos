078602d4467170330f1faaedebfeef009c05830a
tscotce <tatyana.celovsky@gmail.com>
2022-09-06 12:18:51 -0400

    Import useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TextField, Button } from "@mui/material";
 import Todo from "./components/Todo";
 
