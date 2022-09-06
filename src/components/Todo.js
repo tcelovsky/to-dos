1df927b7e1accefdea40cf3cb605462fb5932c5e
tscotce <tatyana.celovsky@gmail.com>
2022-09-06 12:03:34 -0400

    Create basic Todo component

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import { List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+
+const Todo = ({ todo }) => {
+  return (
+    <List className="todo_list">
+      <ListItem>
+        <ListItemAvatar />
+        <ListItemText primary={todo} secondary={todo} />
+      </ListItem>
+    </List>
+  );
+};
