7d7880665ae23bc82112bbefa87bcc95b68d3745
tscotce <tatyana.celovsky@gmail.com>
2022-09-10 13:37:49 -0400

    Import firebase

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,7 @@
 import { List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { db } from "../firebase.js";
+import { doc, deleteDoc } from "firebase/firestore";
 
 const Todo = ({ todo }) => {
   return (
