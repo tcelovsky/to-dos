0458baa002ede481d204e3955900547af0df3747
tscotce <tatyana.celovsky@gmail.com>
2022-09-20 11:24:39 -0400

    Create basic Item component that will display each list item

---
+++ src/components/Item.js
@@ -0,0 +1,15 @@
+import { List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { db } from "../firebase.js";
+import { doc, deleteDoc } from "firebase/firestore";
+
+const Item = ({ item }) => {
+  return (
+    <ListItem>
+      <ListItemAvatar />
+      <ListItemText secondary={item.listName.todo} />
+    </ListItem>
+  );
+};
+
+export default Item;
